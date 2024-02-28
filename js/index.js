d8fe75d18d9880758ca4d4193fc65f1e20627301
mmirazz <mehedimiraz900@gmail.com>
2024-02-28 17:37:45 +0600

    final editing file added

--- js/index.js
+++ js/index.js
@@ -12,16 +12,15 @@ const displayPhone = (phones, isShowAll) => {
   // clear phone container cards before adding new cards
   phoneContainer.textContent = "";
 
-  // display show all button if there are more then 12 phones 
-  const showAllContainer = document.getElementById('show-all-container')
-  if( phones.length > 12 && !isShowAll){
-    showAllContainer.classList.remove('hidden')
+  // display show all button if there are more then 12 phones
+  const showAllContainer = document.getElementById("show-all-container");
+  if (phones.length > 12 && !isShowAll) {
+    showAllContainer.classList.remove("hidden");
+  } else {
+    showAllContainer.classList.add("hidden");
   }
-  else{
-    showAllContainer.classList.add('hidden')
-  }
-  // display only first 12 phones if not show all 
-  if (!isShowAll){
+  // display only first 12 phones if not show all
+  if (!isShowAll) {
     phones = phones.slice(0, 12);
   }
 
@@ -36,45 +35,67 @@ const displayPhone = (phones, isShowAll) => {
   <h2 class="card-title text-center">${phone.phone_name}</h2>
   <p>There are many variations of passages of available, but the majority have suffered</p>
   <div class="card-actions justify-center">
-    <button onclick ="handelShowDetail('${phone.slug}')" class="btn btn-primary">Show Details</button>
+    <button onclick ="handelShowDetail('${phone.slug}');" class="btn btn-primary">Show Details</button>
   </div>
 </div>`;
     // appendChild
     phoneContainer.appendChild(phoneCard);
   });
   // hide loading spinner
-  toggleLoadingSpinner(false)
-
+  toggleLoadingSpinner(false);
 };
 
-const handelShowDetail = async (id) =>{
+const handelShowDetail = async (id) => {
+  const res = await fetch(
+    `https://openapi.programming-hero.com/api/phone/${id}`
+  );
+  const data = await res.json();
+  const phone = data.data;
+  showPhoneDetail(phone);
+};
+const showPhoneDetail = (phone) => {
+  const phoneName = document.getElementById("phone-name");
+  phoneName.innerText = phone.name;
 
-    const res = await fetch(`https://openapi.programming-hero.com/api/phone/${id}`)
-    const data = await res.json()
+  const showDetailContainer = document.getElementById("show-detail-container");
 
-    console.log('click show details', id)
+  showDetailContainer.innerHTML = `
+    <img class="flex justify-center my-7 " src = "${phone.image} " alt ""/>
+    <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
+    <p><span class="text-lg font-semibold text-black">storage:</span>${phone?.mainFeatures?.storage}</p>
+    <p><span class="text-lg font-semibold text-black">storage:</span>${phone?.mainFeatures?.storage}</p>
+    <p><span class="text-lg font-semibold text-black">Display size :</span>${phone?.mainFeatures?.displaySize}</p>
+    <p><span class="text-lg font-semibold text-black">ChipSet:</span>${phone?.mainFeatures?.chipSet}</p>
+    <p><span class="text-lg font-semibold text-black">Memory:</span>${phone?.mainFeatures?.memory}</p>
+    <p><span class="text-lg font-semibold text-black">Slug:</span>${phone?.slug}</p>
+    <p><span class="text-lg font-semibold text-black">Release date : </span>${phone?.releaseDate}</p>
+    <P><span class="text-lg font-semibold text-black">Brand:</span>${phone?.brand}</P>
+    <P><span class="text-lg font-semibold text-black">GPS :</span>${phone?.others?.GPS}</P>
+    
+    `;
 
-}
+  // show the modal
+  show_details_modal.showModal();
+};
+
 // handel search button
 const handlerSearch = (isShowAll) => {
-    toggleLoadingSpinner(true)
+  toggleLoadingSpinner(true);
   const searchField = document.getElementById("search-field");
   const searchText = searchField.value;
   // console.log(searchText)
   loadPhone(searchText, isShowAll);
 };
 
+const toggleLoadingSpinner = (isLoading) => {
+  const loadingSpinner = document.getElementById("loading-spinner");
+  if (isLoading) {
+    loadingSpinner.classList.remove("hidden");
+  } else {
+    loadingSpinner.classList.add("hidden");
+  }
+};
 
-const toggleLoadingSpinner = (isLoading)=> {
-    const loadingSpinner = document.getElementById('loading-spinner')
-    if(isLoading){
-        loadingSpinner.classList.remove('hidden')
-    } else{
-        loadingSpinner.classList.add('hidden')
-    }
-  
-}
-
-const handlerShowAll = ()=>{
-    handlerSearch(true)
-}
+const handlerShowAll = () => {
+  handlerSearch(true);
+};
